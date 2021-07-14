4511481f17d8bf584f13ddad5308589bbd421653
KeiPro <keiprogramo@gmail.com>
2021-07-15 01:48:09 +0900

    Color Change

--- app/src/main/java/com/example/accountbookproject/MainActivity.java
+++ app/src/main/java/com/example/accountbookproject/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ab_main);
+        //setContentView(R.layout.activity_calendar);
 
         InitMainActivity();
     }
